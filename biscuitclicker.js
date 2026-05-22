9f2027666eecb0c8faa88545ca117a1c5dc34a9a
CrashoutKing123 <benjamin.bourgade29@student.tamdistrict.org>
2026-05-22 02:01:43 +0000

    W LOCK IN added all the way up to agricultural property

--- biscuitclicker.js
+++ biscuitclicker.js
@@ -1,10 +1,179 @@
 let point = document.querySelector('.point-cost')
 let parsedPoint = parseFloat(point.innerHTML)
+let ppsText = document.getElementById("pps-text")
+let pps = 0;
+let totalBiscuitsBaked = 0; 
 
-//put upgrade code here lil bro
+let pointerFingerCount = 0;
+const pointerFingerBaseCost = 15;
+let pointerFingerCurrentCost = pointerFingerBaseCost;
+const pointerFingerPPSContribution = 0.1;
+
+let elderlyManCount = 0;
+const elderlyManBaseCost = 100;
+let elderlyManCurrentCost = elderlyManBaseCost;
+const elderlyManPPSContribution = 1.0;
+
+let agriculturalPropertyCount = 0;
+const agriculturalPropertyBaseCost = 1100; 
+let agriculturalPropertyCurrentCost = agriculturalPropertyBaseCost;
+const agriculturalPropertyPPSContribution = 8.0; 
+
+function buyPointerFinger() {
+    if (parsedPoint >= pointerFingerCurrentCost) {
+        parsedPoint -= pointerFingerCurrentCost;
+        point.innerHTML = Math.floor(parsedPoint);
+        
+        pointerFingerCount++;
+        pointerFingerCurrentCost = Math.ceil(pointerFingerBaseCost * Math.pow(1.15, pointerFingerCount));
+        
+        calculateTotalPPS();
+        updateShopUI(); 
+    } else {
+        console.log("Not enough biscuits, lil bro!");
+    }
+}
+
+function buyElderlyMan() {
+    if (parsedPoint >= elderlyManCurrentCost) {
+        parsedPoint -= elderlyManCurrentCost;
+        point.innerHTML = Math.floor(parsedPoint);
+        
+        elderlyManCount++;
+        elderlyManCurrentCost = Math.ceil(elderlyManBaseCost * Math.pow(1.15, elderlyManCount));
+        
+        calculateTotalPPS();
+        updateShopUI();
+    } else {
+        console.log("The elderly men require more biscuits!");
+    }
+}
+
+function buyAgriculturalProperty() {
+    if (parsedPoint >= agriculturalPropertyCurrentCost) {
+        parsedPoint -= agriculturalPropertyCurrentCost;
+        point.innerHTML = Math.floor(parsedPoint);
+        
+        agriculturalPropertyCount++;
+        agriculturalPropertyCurrentCost = Math.ceil(agriculturalPropertyBaseCost * Math.pow(1.15, agriculturalPropertyCount));
+        
+        calculateTotalPPS();
+        updateShopUI();
+    } else {
+        console.log("Acquiring farmland demands more biscuits!");
+    }
+}
+
+function checkUnlocks() {
+    let pointerFingerElement = document.getElementById("shop-item-pointer-finger");
+    if (pointerFingerElement) {
+        if (totalBiscuitsBaked >= pointerFingerBaseCost) {
+            pointerFingerElement.classList.add("visible");
+        }
+        if (pointerFingerCount > 0) {
+            pointerFingerElement.classList.add("fully-unlocked");
+        }
+    }
+
+    let elderlyManElement = document.getElementById("shop-item-elderly-man");
+    if (elderlyManElement) {
+        if (totalBiscuitsBaked >= elderlyManBaseCost) {
+            elderlyManElement.classList.add("visible"); 
+        }
+        if (elderlyManCount > 0) {
+            elderlyManElement.classList.add("fully-unlocked"); 
+        }
+    }
+
+    let agPropertyElement = document.getElementById("shop-item-agricultural-property");
+    if (agPropertyElement) {
+        if (totalBiscuitsBaked >= agriculturalPropertyBaseCost) {
+            agPropertyElement.classList.add("visible");
+        }
+        if (agriculturalPropertyCount > 0) {
+            agPropertyElement.classList.add("fully-unlocked");
+        }
+    }
+}
+
+function calculateTotalPPS() {
+    pps = (pointerFingerCount * pointerFingerPPSContribution) + 
+          (elderlyManCount * elderlyManPPSContribution) + 
+          (agriculturalPropertyCount * agriculturalPropertyPPSContribution);
+          
+    ppsText.innerHTML = pps % 1 === 0 ? pps : pps.toFixed(1);
+}
+
+function updateShopUI() {
+    let fingerPriceTag = document.getElementById("pointer-finger-price");
+    let fingerCountTag = document.getElementById("pointer-finger-count");
+    if (fingerPriceTag) fingerPriceTag.innerHTML = pointerFingerCurrentCost;
+    if (fingerCountTag) fingerCountTag.innerHTML = pointerFingerCount;
+
+    let elderlyPriceTag = document.getElementById("elderly-man-price");
+    let elderlyCountTag = document.getElementById("elderly-man-count");
+    if (elderlyPriceTag) elderlyPriceTag.innerHTML = elderlyManCurrentCost;
+    if (elderlyCountTag) elderlyCountTag.innerHTML = elderlyManCount;
+
+    let agPriceTag = document.getElementById("agricultural-property-price");
+    let agCountTag = document.getElementById("agricultural-property-count");
+    if (agPriceTag) agPriceTag.innerHTML = agriculturalPropertyCurrentCost;
+    if (agCountTag) agCountTag.innerHTML = agriculturalPropertyCount;
+}
 
 function incrementPoints() {
     parsedPoint += 1;
-    point.innerHTML = parsedPoint
-    point.innerHTML = Math.round(point.innerHTML)
+    totalBiscuitsBaked += 1; 
+    point.innerHTML = Math.floor(parsedPoint);
+    checkUnlocks(); 
 }
+
+const ticksPerSecond = 30;
+
+setInterval(() => {
+    if (pps > 0) {
+        let ppsPerTick = pps / ticksPerSecond;
+        parsedPoint += ppsPerTick;
+        totalBiscuitsBaked += ppsPerTick; 
+        point.innerHTML = Math.floor(parsedPoint);
+    }
+    checkUnlocks(); 
+}, 1000 / ticksPerSecond);
+
+const modal = document.getElementById("naming-modal");
+const nameInput = document.getElementById("bakery-name-input");
+const nameDisplay = document.getElementById("bakery-name");
+
+const randomPrefixes = ["Magic", "Golden", "Grandma", "Fancy", "Tasty", "Power", "Lucky", "Happy", "Crispy"];
+const randomSuffixes = ["Turtle", "Baker", "Biscuit", "Cookie", "Monster", "Hero", "Star", "Knight", "Captain"];
+
+function openNamingModal() {
+    modal.style.display = "flex";
+    nameInput.value = nameDisplay.innerText;
+    nameInput.focus();
+    nameInput.select();
+}
+
+function closeNamingModal() {
+    modal.style.display = "none";
+}
+
+function confirmBakeryName() {
+    let cleanName = nameInput.value.trim();
+    if (cleanName !== "") {
+        nameDisplay.innerText = cleanName;
+    }
+    closeNamingModal();
+}
+
+function randomBakeryName() {
+    let randomPref = randomPrefixes[Math.floor(Math.random() * randomPrefixes.length)];
+    let randomSuff = randomSuffixes[Math.floor(Math.random() * randomSuffixes.length)];
+    nameInput.value = randomPref + " " + randomSuff;
+}
+
+nameInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        confirmBakeryName();
+    }
+});
